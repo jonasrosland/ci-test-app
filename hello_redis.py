d6b69e86494a72469ec3620b96c7afe1e1d867fe
jonasrosland <jonas.rosland@gmail.com>
2015-06-10 13:35:41 -0400

    redis

---
+++ hello_redis.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import redis
+import os
+import urlparse
+import json
+
+
+app = Flask(__name__)
+rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
+credentials = rediscloud_service['credentials']
+r = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
+
+@app.route('/')
+def hello():
+   r.incr('hits')
+   return 'Hello World! I have been seen %s times.\n' % r.get('hits')
+
+
+port = os.getenv('VCAP_APP_PORT', '5000')
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=int(port))
